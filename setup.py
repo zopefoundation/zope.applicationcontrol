a5a71b377f0de0d3bd2f156c0190951d1980c04a
Tres Seaver <tseaver@palladion.com>
2010-06-02 20:54:07 +0000

    Add tag 3.4.1

---
+++ setup.py
@@ -0,0 +1,71 @@
+##############################################################################
+#
+# Copyright (c) 2006-2007 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Setup for zope.annotation package
+
+$Id$
+"""
+
+import os
+
+from setuptools import setup, find_packages
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+long_description = (
+    read('README.txt')
+    + '\n' +
+    read('CHANGES.txt')
+    + '\n' +
+    'Detailed Documentation\n'
+    '**********************\n'
+    + '\n' +
+    read('src', 'zope', 'annotation', 'README.txt') 
+    )
+
+setup(
+    name = 'zope.annotation',
+    version = '3.4.1',
+    url = 'http://pypi.python.org/pypi/zope.annotation',
+    license = 'ZPL 2.1',
+    description = 'Zope annotation',
+    author = 'Zope Corporation and Contributors',
+    author_email = 'zope3-dev@zope.org',
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Zope Public License',
+        'Programming Language :: Python',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Software Development',
+        ],
+    long_description = long_description,
+    packages = find_packages('src'),
+    package_dir = {'': 'src'},
+    namespace_packages = ['zope',],
+    install_requires = ['setuptools',
+                        'zope.interface',
+                        'zope.component',
+                        'zope.location>=3.4.0b1.dev-r78903',
+                        'zope.proxy',
+                        ],
+    extras_require = dict(
+        test = ['zope.testing',
+                'ZODB3'],
+        ),
+    include_package_data = True,
+    zip_safe = False,
+    )
