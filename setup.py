0bab33fa9347b9e2addf48f4cf92d81ca132a3d4
Jason Madden <jamadden@gmail.com>
2017-05-03 08:56:38 -0500

    Back to development: 4.1.1

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ tests_require = [
 
 setup(
     name='zope.applicationcontrol',
-    version='4.1.0',
+    version='4.1.1.dev0',
     author='Zope Foundation and Contributors',
     author_email='zope-dev@zope.org',
     description='Zope applicationcontrol',
