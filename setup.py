b5faed0ba568158f91766dc90c4944f641b89082
Marius Gedminas <marius@gedmin.as>
2018-10-19 00:59:00 +0300

    Switch to new Framework :: Zope :: 3 classifier
    See https://github.com/zopefoundation/z3c.authviewlet/pull/6#discussion_r226414566
    for the background behind this.

--- setup.py
+++ setup.py
@@ -74,7 +74,7 @@ setup(
         'Natural Language :: English',
         'Operating System :: OS Independent',
         'Topic :: Internet :: WWW/HTTP',
-        'Framework :: Zope3',
+        'Framework :: Zope :: 3',
     ],
     url='http://github.com/zopefoundation/zope.applicationcontrol',
     extras_require={
