4f6aeabb1b2aba865096ec26258d0578bc20e982
Michael Howitz <mh@gocept.com>
2022-06-24 17:33:45 +0200

    Back to development: 4.4

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ tests_require = [
 
 setup(
     name='zope.applicationcontrol',
-    version='4.3',
+    version='4.4.dev0',
     author='Zope Foundation and Contributors',
     author_email='zope-dev@zope.org',
     description='Zope applicationcontrol',
