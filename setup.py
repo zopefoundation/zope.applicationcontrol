0c18f5019de0c011cbf9bb0d336e40805e9368c9
Michael Howitz <icemac@gmx.net>
2025-02-13 10:13:47 +0100

    Preparing release 5.1

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ tests_require = [
 
 setup(
     name='zope.applicationcontrol',
-    version='5.1.dev0',
+    version='5.1',
     author='Zope Foundation and Contributors',
     author_email='zope-dev@zope.dev',
     description='Zope applicationcontrol',
